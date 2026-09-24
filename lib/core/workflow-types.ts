d844e65110d5215ed9ce33dc0a33a744e4846280
Michelle Giray <mitchie028@gmail.com>
2026-09-24 09:04:31 +0800

    Delete lib directory

--- lib/core/workflow-types.ts
+++
@@ -1,7 +0,0 @@
-export type EngineResult<T = unknown> = {
-  ok: boolean
-  processed: number
-  errors: number
-  data?: T
-  error?: string | null
-}
--- lib/queue/redis.ts
+++
@@ -1,6 +0,0 @@
-import { Redis } from "ioredis"
-
-export const redis = new Redis(process.env.REDIS_URL!, {
-  maxRetriesPerRequest: null,
-  enableReadyCheck: false,
-})
